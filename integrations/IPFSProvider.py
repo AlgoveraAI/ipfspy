5e1a16ad3f005ed9a41ee93770c9d8e347d773a7
Jakub Smékal <jakub.smekal@gmail.com>
2022-06-23 08:33:19 +0100

    WIP: refactored gw, added all functions

--- integrations/IPFSProvider.py
+++ integrations/IPFSProvider.py
@@ -1,4 +1,5 @@
 import hub
+import requests
 
 from requests.exceptions import HTTPError
 from ipfspy.utils import GATEWAYS_API_READ, GATEWAYS_API_WRITE, parse_error_message, parse_response, get_coreurl
@@ -8,8 +9,7 @@ class IPFSProvider(hub.core.storage.provider.StorageProvider):
     def __init__(self) -> None:
         """Initialize the object, assign credentials if required."""
         super().__init__()
-        self.gateway = IPFSGateway()
-        self.fs = IPFSFileSystem()
+        self.fs = IPFSFileSystem(local=False, coreurl='https://ipfs.infura.io:5001')
 
 
     def __getitem__(self, cid, **kwargs):
@@ -29,19 +29,37 @@ class IPFSProvider(hub.core.storage.provider.StorageProvider):
 
     def __setitem__(self, path, value):
         """Sets the object present at the path with the value"""
-        return self.gateway.apipost("add", path)
+        return self.fs.gw.apipost("add", path)
 
 
     def __delitem__(self, path):
         """Delete the object present at the path."""
-        return
+
+        params = {
+            'arg': path,
+        }
+
+        response = requests.post('https://ipfs.infura.io:5001/api/v0/pin/rm', params=params)
+        return response
 
 
-    def __iter__(self):
+    def __iter__(self, path):
         """Generator function that iterates over the keys of the mapper"""
-        return self.fs.ls() # which cid to use here?
+        params = {
+            'arg': path,
+            'long': 'false',
+            'U': 'false',
+        }
+
+        response = requests.post('http://127.0.0.1:5001/api/v0/files/ls', params=params)
+        return response
 
 
-    def __len__(self):
-        """Returns the number of files present in the directory at the root of the mapper"""
-        return
+
+    def __len__(self, path):
+        """Returns the the information of a raw IPFS block"""
+        params = {
+            'arg': path,
+        }
+
+        response = requests.post('http://127.0.0.1:5001/api/v0/block/stat', params=params)
